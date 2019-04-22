00f1ed208772af08bbff8985d8bab9688b1c65e4
Sergey Gladkovskiy <smgladkovskiy@gmail.com>
2019-04-23 00:37:06 +0300

    throw away gin and gorm loggers (will be in separate packages). tests, refactoring

--- go.mod
+++ go.mod
@@ -6,19 +6,7 @@ require (
 	github.com/certifi/gocertifi v0.0.0-20190415143156-92f724a62f3e // indirect
 	github.com/evalphobia/logrus_sentry v0.8.2
 	github.com/getsentry/raven-go v0.2.0 // indirect
-	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
-	github.com/gin-gonic/gin v1.3.0
-	github.com/golang/protobuf v1.3.1 // indirect
-	github.com/json-iterator/go v1.1.6 // indirect
-	github.com/mattn/go-isatty v0.0.7 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/sirupsen/logrus v1.4.1
-	github.com/stretchr/testify v1.2.2
-	github.com/ugorji/go v1.1.4 // indirect
-	golang.org/x/net v0.0.0-20190419010253-1f3472d942ba // indirect
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
-	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
+	github.com/stretchr/testify v1.3.0
 )
---
+++ logger_test.go
@@ -0,0 +1,91 @@
+package logs
+
+import (
+	"testing"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+func logConfig(withSenrty bool) *Config {
+	s := &SentryConfig{
+		Enable:          true,
+		Stage:           "test",
+		DSN:             "https://xxx@sentry.io/yyy",
+		ResponseTimeout: 0,
+		MinlLogLevel:    "error",
+		StackTrace: StackTraceConfig{
+			Enable: true,
+		},
+	}
+	cfg := &Config{
+		Level:  "info",
+		Format: "text",
+	}
+	if withSenrty {
+		cfg.Sentry = s
+	}
+	return cfg
+}
+
+// I just want to test it before pushing. Don't know how to test it in right way, sorry )
+func TestNewLogger(t *testing.T) {
+	t.Run("Construct new logger", func(t *testing.T) {
+		cfg := logConfig(false)
+		l, err := NewLogger(cfg)
+		assert.NoError(t, err)
+		assert.Equal(t, logrus.InfoLevel, l.GetLevel())
+	})
+
+	t.Run("log format", func(t *testing.T) {
+		cfg := logConfig(false)
+		cfg.Format = "du'soley"
+		l, err := NewLogger(cfg)
+		assert.NoError(t, err)
+		assert.Equal(t, &logrus.JSONFormatter{TimestampFormat: time.RFC3339}, l.Formatter)
+	})
+
+	t.Run("good sentry config", func(t *testing.T) {
+		cfg := logConfig(true)
+		l, err := NewLogger(cfg)
+		assert.NoError(t, err)
+		assert.Equal(t, len(l.Hooks), len(sentryLevels[:getLoggerLevel(cfg)]))
+	})
+
+	t.Run("bad sentry config", func(t *testing.T) {
+		cfg := logConfig(true)
+		cfg.Sentry.DSN = "bad dsn"
+		_, err := NewLogger(cfg)
+		assert.Error(t, err)
+	})
+
+}
+
+func TestGetLoggerLeve(t *testing.T) {
+	t.Run("existing log level", func(t *testing.T) {
+		cfg := logConfig(false)
+		ll := getLoggerLevel(cfg)
+		assert.Equal(t, logrus.InfoLevel, ll)
+
+	})
+	t.Run("not existing log level", func(t *testing.T) {
+		cfg := logConfig(false)
+		cfg.Level = "paranoia"
+		ll := getLoggerLevel(cfg)
+		assert.Equal(t, logrus.WarnLevel, ll)
+	})
+}
+
+func TestSentryStage(t *testing.T) {
+	t.Run("stage is set", func(t *testing.T) {
+		cfg := logConfig(true)
+		assert.Equal(t, "test", getSTAGE(cfg))
+	})
+
+	t.Run("stage is not set", func(t *testing.T) {
+		cfg := logConfig(true)
+		cfg.Sentry.Stage = ""
+		assert.Equal(t, defaultSTAGE, getSTAGE(cfg))
+	})
+}
